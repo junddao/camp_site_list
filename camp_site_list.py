2c80052af7f2e3fe8389efb1d8942c4dddcc5301
junddao <y2hunter@gmail.com>
2023-08-30 16:40:12 +0900

    Upt.

--- camp_site_list.py
+++ camp_site_list.py
@@ -3,18 +3,19 @@ from bs4 import BeautifulSoup
 import json
 import re
 
-
 def crawl_camping_sites():
-    url = "https://gocamping.or.kr/bsite/camp/info/list.do"
-    params = {
-        "searchKrwd": "",  # 원하는 키워드로 필터링하려면 이 부분을 채워넣으세요.
-        "listOrdrTrget": "last_updusr_pnttm",  # 최근 업데이트 순으로 정렬
-        "listOrdr": "desc",  # 내림차순 정렬
-        "pageIndex": 1,  # 페이지 인덱스
-        "listRowPerPage": 2,  # 페이지당 보여줄 개수 (100으로 설정)
-    }
 
-    response = requests.get(url, params=params)
+    url = "https://gocamping.or.kr/bsite/camp/info/list.do?pageUnit=3664"
+    # params = {
+    #     "searchKrwd": "",  # 원하는 키워드로 필터링하려면 이 부분을 채워넣으세요.
+    #     "listOrdrTrget": "last_updusr_pnttm",  # 최근 업데이트 순으로 정렬
+    #     "listOrdr": "desc",  # 내림차순 정렬
+    #     "pageIndex": 1,  # 페이지 인덱스
+    #     "pageUnit": 3524,  # 페이지당 보여줄 개수 (100으로 설정)
+    # }
+
+    response = requests.get(url)
+    # response = requests.get(url, params=params)
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, "html.parser")
         camping_sites = soup.select(
@@ -25,15 +26,39 @@ def crawl_camping_sites():
         # camping_sites = ul_element.find_all("li")
         print(len(camping_sites))
         camping_list = []
+        idx = 0
         for camping_site in camping_sites:
             site_url = camping_site.find("a")["href"]
             # print(camping_site.find("a", {"class": "dc_none"})["href"])
+            
             e_addr = camping_site.find("li", {"class": "addr"})  # 클래스 이름이 "addr"인 경우
             e_call = camping_site.find(
                 "li", {"class": "call_num"}
             )  # 클래스 이름이 "call_num"인 경우
             site_address = e_addr.text if e_addr else ""
             site_phone = e_call.text if e_call else ""
+
+            pattern = r"\[([^]]+)\]"
+
+            temp = camping_site.select_one("div.camp_cont > h2.camp_tt > a").text
+            idx = idx + 1
+            print(idx)
+            match = re.search(pattern, temp)
+
+            if match:
+                site_address_all = match.group(1)  # 첫 번째 그룹의 값 (하하하)
+                site_address_filter1, site_address_filter2 = site_address_all.split(" ")
+                
+                # site_address_filter2 = match.group(2)  # 두 번째 그룹의 값 (호호호)
+                
+                # print(site_address_filter2)
+            else:
+                print("일치하는 패턴을 찾을 수 없습니다.")
+
+
+
+
+     
 
             # site_url_element = camping_site.select_one("div.camp_cont > h2.camp_tt > a")
 
@@ -43,15 +68,17 @@ def crawl_camping_sites():
             # site_url_element = camping_site.find("a", {"class": "dc_none"})
             # site_url = site_url_element["href"] if site_url_element else ""
             # print("url :" + site_url)
-
-            site_response = requests.get("https://gocamping.or.kr" + site_url)
+            try:
+                site_response = requests.get("https://gocamping.or.kr" + site_url)
+            except:
+                continue
             if site_response.status_code == 200:
                 site_soup = BeautifulSoup(site_response.content, "html.parser")
                 links = site_soup.find_all("a", attrs={"title": "새창열림"})
                 for link in links:
                     href = link.get("href")
 
-                site_homepage = href
+                site_homepage = href if href else ""
 
                 site_name_element = site_soup.select_one("p.camp_name")
                 site_name = (
@@ -60,15 +87,26 @@ def crawl_camping_sites():
                     else ""
                 )
 
+                site_image_element = site_soup.select_one("div.img_b > img")
+                site_image = (
+                    site_soup.select_one("div.img_b > img")["src"]
+                    if site_image_element
+                    else ""
+                )
+                
+
             else:
                 site_name = ""
                 site_homepage = ""
 
             camping = {
-                "캠핑장명": site_name,
-                "주소": site_address,
-                "홈페이지": site_homepage,
-                "전화번호": site_phone,
+                "name": site_name,
+                "address": site_address,
+                "region1": site_address_filter1,
+                "region2": site_address_filter2,
+                "homePage": site_homepage,
+                "phone": site_phone,
+                "image": site_image,
             }
 
             camping_list.append(camping)
@@ -83,5 +121,5 @@ def crawl_camping_sites():
 crawl_camping_sites()
 
 
-if __name__ == "__main__":
-    crawl_camping_sites()
+# if __name__ == "__main__":
+#     crawl_camping_sites()
